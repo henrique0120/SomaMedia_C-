72f781bc3410650ba2054ca68a57cfdf884d112a
Henrique de Souza <henrique.sl959@hotmail.com>
2024-01-17 18:52:58 -0300

    Programa simples em C# que mostra a soma e a média de uma lista de números digitados

---
+++ SomaMedia/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Entre 3 e 10 números, digite quantos você deseja colocar na lista: ");
+int n = int.Parse(Console.ReadLine());
+
+List<float> numbers = new List<float>();
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Digite um número: ");
+    string input = Console.ReadLine();
+    if (float.TryParse(input, out float n1))
+    {
+        numbers.Add(n1);
+    }
+    else
+    {
+        Console.WriteLine("Entrada inválida. Por favor, insira números válidos");
+        i--;
+    }
+}
+
+static float calcularSoma(List<float> numbers)
+{
+    float sum = numbers.Sum();
+    return sum;
+}
+
+static float calcularMedia(List<float> numbers)
+{
+    float media = numbers.Average();
+    return media;
+}
+
+
+Console.WriteLine($"A soma dos valores digitados é {calcularSoma(numbers)} e a média é {calcularMedia(numbers)}");
